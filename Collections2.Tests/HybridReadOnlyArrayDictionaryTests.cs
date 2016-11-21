2cd12b57d88f14d93a8b809a7dd9a5a8a7d6d1e9
Aleksey Berezan <aleksey.berezan@gmail.com>
2016-11-20 19:16:35 -0500

    Initial commit

---
+++ Collections2.Tests/HybridReadOnlyArrayDictionaryTests.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Collections2.Tests
+{
+    [TestFixture]
+    public class HybridReadOnlyArrayDictionaryTests
+    {
+        [Test]
+        public void Returns_items_from_empty_dictionary()
+        {
+            var orig = CreateDictionary(0);
+
+            // act
+            var d = new HybridReadOnlyArrayDictionary<int, int>(orig);
+
+            // assert
+            TestDictionary(d, 0);
+        }
+
+        [Test]
+        public void Returns_items_from_small_dictionary()
+        {
+            var orig = CreateDictionary(3);
+
+            // act
+            var d = new HybridReadOnlyArrayDictionary<int, int>(orig);
+
+            // assert
+            TestDictionary(d, 3);
+        }
+
+        [Test]
+        public void Returns_items_from_medium_dictionary()
+        {
+            var orig = CreateDictionary(6);
+
+            // act
+            var d = new HybridReadOnlyArrayDictionary<int, int>(orig);
+
+            // assert
+            TestDictionary(d, 6);
+        }
+
+        [Test]
+        public void Returns_items_from_large_dictionary()
+        {
+            var orig = CreateDictionary(16);
+
+            // act
+            var d = new HybridReadOnlyArrayDictionary<int, int>(orig);
+
+            // assert
+            TestDictionary(d, 16);
+        }
+
+        #region Helper Methods
+
+        private static IDictionary<int, int> CreateDictionary(int numItems)
+        {
+            Dictionary<int, int> orig = new Dictionary<int, int>(numItems);
+            for (int i = 0; i < numItems; i++)
+            {
+                orig.Add(i, i);
+            }
+
+            return orig;
+        }
+
+        private static void TestDictionary(HybridReadOnlyArrayDictionary<int, int> d, int numItems)
+        {
+            TestCount(d, numItems);
+            TestKeys(d, numItems);
+            TestValues(d, numItems);
+            TestItemIndexer(d, numItems);
+            TestContainsKey(d, numItems);
+            TestTryGetValue(d, numItems);
+            TestGetEnumerator(d, numItems);
+            TestGetEnumeratorExplicit(d, numItems);
+        }
+
+        private static void TestGetEnumeratorExplicit(HybridReadOnlyArrayDictionary<int, int> d, int numItems)
+        {
+            IEnumerable e = d;
+            IEnumerator enumerator = e.GetEnumerator();
+
+            int[] keys = new int[numItems];
+            int[] values = new int[numItems];
+
+            int i = 0;
+            while (enumerator.MoveNext())
+            {
+                keys[i] = ((KeyValuePair<int, int>)enumerator.Current).Key;
+                values[i] = ((KeyValuePair<int, int>)enumerator.Current).Value;
+                i++;
+            }
+
+            TestArrayAscendance(numItems, keys);
+            TestArrayAscendance(numItems, values);
+        }
+
+        private static void TestGetEnumerator(HybridReadOnlyArrayDictionary<int, int> d, int numItems)
+        {
+            TestArrayAscendance(numItems, d.Select(x => x.Key).ToArray());
+            TestArrayAscendance(numItems, d.Select(x => x.Value).ToArray());
+        }
+
+        private static void TestContainsKey(HybridReadOnlyArrayDictionary<int, int> d, int numItems)
+        {
+            for (int i = 0; i < numItems; i++)
+            {
+                Assert.IsTrue(d.ContainsKey(i));
+            }
+
+            Assert.IsFalse(d.ContainsKey(-1));
+        }
+
+        private static void TestItemIndexer(HybridReadOnlyArrayDictionary<int, int> d, int numItems)
+        {
+            for (int i = 0; i < numItems; i++)
+            {
+                Assert.AreEqual(i, d[i]);
+            }
+
+            Exception exc = null;
+            try
+            {
+                int ignore = d[-1];
+            }
+            catch (KeyNotFoundException e)
+            {
+                exc = e;
+            }
+
+            Assert.IsNotNull(exc);
+            Assert.AreEqual(typeof(KeyNotFoundException), exc.GetType());
+        }
+
+        private static void TestTryGetValue(HybridReadOnlyArrayDictionary<int, int> d, int numItems)
+        {
+            for (int i = 0; i < numItems; i++)
+            {
+                int value;
+                bool success = d.TryGetValue(i, out value);
+
+                Assert.IsTrue(success);
+                Assert.AreEqual(i, value);
+            }
+
+            int value2;
+            bool success2 = d.TryGetValue(-1, out value2);
+            Assert.IsFalse(success2);
+            Assert.AreEqual(value2, default(int));
+        }
+
+        private static void TestCount(HybridReadOnlyArrayDictionary<int, int> d, int numItems)
+        {
+            Assert.AreEqual(d.Count, numItems);
+        }
+
+        private static void TestKeys(HybridReadOnlyArrayDictionary<int, int> d, int numItems)
+        {
+            TestArrayAscendance(numItems, d.Keys.ToArray());
+        }
+
+        private static void TestValues(HybridReadOnlyArrayDictionary<int, int> d, int numItems)
+        {
+            TestArrayAscendance(numItems, d.Values.ToArray());
+        }
+
+        private static void TestArrayAscendance(int numItems, int[] array)
+        {
+            Assert.AreEqual(numItems, array.Length);
+            for (int i = 0; i < numItems; i++)
+            {
+                Assert.AreEqual(i, array[i]);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Collections2/HybridReadOnlyArrayDictionary.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collections2
+{
+    public class HybridReadOnlyArrayDictionary<TKey, TValue> : IReadOnlyDictionary<TKey, TValue>
+    {
+        private const int MaxArrayItems = 6;
+
+        private readonly TKey[] _keys;
+        private readonly TValue[] _values;
+        private readonly int _arrayCount;
+
+        private readonly Dictionary<TKey, TValue> _dictionary;
+
+        public HybridReadOnlyArrayDictionary(IEnumerable<KeyValuePair<TKey, TValue>> pairs)
+        {
+            _keys = new TKey[MaxArrayItems];
+            _values = new TValue[MaxArrayItems];
+
+            int count = 0;
+            foreach (var pair in pairs)
+            {
+                if (count < MaxArrayItems)
+                {
+                    _keys[count] = pair.Key;
+                    _values[count] = pair.Value;
+                }
+                else if (count == MaxArrayItems)
+                {
+                    _dictionary = new Dictionary<TKey, TValue>(MaxArrayItems * 2);
+                    for (int i = 0; i < MaxArrayItems; i++)
+                    {
+                        _dictionary.Add(_keys[i], _values[i]);
+                    }
+
+                    _keys = null;
+                    _values = null;
+
+                    _dictionary.Add(pair.Key, pair.Value);
+                }
+                else if (count > MaxArrayItems)
+                {
+                    _dictionary.Add(pair.Key, pair.Value);
+                }
+
+                count++;
+            }
+
+            _arrayCount = _keys == null ? -1 : count;
+            if (_arrayCount != -1 && _arrayCount < MaxArrayItems - 1)
+            {
+                var tKeys = new TKey[_arrayCount];
+                Array.Copy(_keys, tKeys, _arrayCount);
+                _keys = tKeys;
+
+                var tValues = new TValue[_arrayCount];
+                Array.Copy(_values, tValues, _arrayCount);
+                _values = tValues;
+            }
+        }
+
+        private bool IsArrayUsed
+        {
+            get { return _keys != null; }
+        }
+
+        public IEnumerable<TKey> Keys
+        {
+            get
+            {
+                if (IsArrayUsed)
+                {
+                    return _keys;
+                }
+
+                return _dictionary.Keys;
+            }
+        }
+
+        public IEnumerable<TValue> Values
+        {
+            get
+            {
+                if (IsArrayUsed)
+                {
+                    return _values;
+                }
+
+                return _dictionary.Values;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                if (IsArrayUsed)
+                {
+                    return _arrayCount;
+                }
+
+                return _dictionary.Count;
+            }
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                if (IsArrayUsed)
+                {
+                    for (int i = 0; i < _arrayCount; i++)
+                    {
+                        if (key.Equals(_keys[i]))
+                        {
+                            return _values[i];
+                        }
+                    }
+
+                    throw new KeyNotFoundException("Key not found: " + key);
+                }
+
+                return _dictionary[key];
+            }
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            if (IsArrayUsed)
+            {
+                for (int i = 0; i < _arrayCount; i++)
+                {
+                    if (key.Equals(_keys[i]))
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            return _dictionary.ContainsKey(key);
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            if (IsArrayUsed)
+            {
+                for (int i = 0; i < _arrayCount; i++)
+                {
+                    if (key.Equals(_keys[i]))
+                    {
+                        value = _values[i];
+                        return true;
+                    }
+                }
+
+                value = default(TValue);
+                return false;
+            }
+
+            return _dictionary.TryGetValue(key, out value);
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            if (IsArrayUsed)
+            {
+                return GetArrayEnumerator();
+            }
+
+            return _dictionary.GetEnumerator();
+        }
+
+        private IEnumerator<KeyValuePair<TKey, TValue>> GetArrayEnumerator()
+        {
+            for (int i = 0; i < _arrayCount; i++)
+            {
+                yield return new KeyValuePair<TKey, TValue>(_keys[i], _values[i]);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            Console.WriteLine("GetEnumerator");
+            return GetEnumerator();
+        }
+    }
+}
